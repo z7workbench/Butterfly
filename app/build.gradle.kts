7ed82cd83256a549056f6335c07f12a68944cb88
ZeroGo <z7workbench@yeah.net>
2023-10-28 22:47:42 +0800

    chore: update AGP & libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "top.z7workbench.butterfly"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "top.z7workbench.butterfly"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -41,17 +41,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -98,4 +98,5 @@ dependencies {
     implementation(libs.accompanist.permissions)
     implementation(libs.accompanist.webview)
     implementation(libs.accompanist.adaptive)
+    implementation(libs.bundles.media3)
 }
